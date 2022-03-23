9e0d8fcd138555231b54341b71fb837bef3bd0f7
Daniel <danigonlinea@gmail.com>
2022-03-23 13:00:39 +0100

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,7 @@ export default {
 
   axios: {
     baseURL: process.env.BASE_URL,
+    proxy: true,
   },
 
   publicRuntimeConfig: {
--- src/api/vueSlots.js
+++ src/api/vueSlots.js
@@ -1,6 +1,6 @@
 import { parse } from 'vue-docgen-api'
 const path = require('path')
-const cors = require('cors')
+/* const cors = require('cors')
 const express = require('express')
 
 const app = express()
@@ -21,4 +21,20 @@ module.exports = app.use(async function (req, res) {
   } catch (error) {
     res.end(`Failed parsing component`)
   }
-})
+}) */
+
+export default async function (req, res, next) {
+  try {
+    const name = 'Button'
+
+    const componentsRootFolder = path.resolve('./node_modules/')
+    const { slots = [] } = await parse(
+      `${componentsRootFolder}/watson-vue/src/components/${name}/${name}.vue`
+    )
+
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(slots))
+  } catch (error) {
+    res.end(`Failed parsing component`)
+  }
+}
