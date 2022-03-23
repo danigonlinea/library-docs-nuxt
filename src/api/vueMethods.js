74f5cabd4761bf2c6e79acae8a727fd2316fe266
Daniel <danigonlinea@gmail.com>
2022-03-23 13:05:12 +0100

    .

--- src/api/vueMethods.js
+++ src/api/vueMethods.js
@@ -7,6 +7,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 module.exports = app.use(async function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+
   try {
     const name = req.body.name
 
